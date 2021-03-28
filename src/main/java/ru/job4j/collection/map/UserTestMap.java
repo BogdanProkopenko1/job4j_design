5d06e82eae0ff63bc0b063bbcb7029af64791b80
BogdanProkopenko1 <integer.args@gmail.com>
2021-03-28 17:20:57 +0500

    2. Без переопределения equals и hashCode [#482500] fix

--- src/main/java/ru/job4j/collection/map/UserTestMap.java
+++ src/main/java/ru/job4j/collection/map/UserTestMap.java
@@ -7,11 +7,10 @@ import java.util.Map;
 public class UserTestMap {
 
     public static void main(String[] args) {
-        User userFirst = new User("Alexander", 0, Calendar.getInstance());
-        User userSecond = new User("Alexander", 0, Calendar.getInstance());
+        User user = new User("Alexander", 0, Calendar.getInstance());
         Map<User, Object> map = new HashMap<>();
-        map.put(userFirst, "First");
-        map.put(userSecond, "Second");
+        map.put(user, "First");
+        map.put(user, "Second");
         System.out.println(map);
     }
 }
