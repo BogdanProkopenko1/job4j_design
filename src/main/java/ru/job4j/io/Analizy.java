a0e7601d6e245c349817e16c997749363153d7fc
BogdanProkopenko1 <integer.args@gmail.com>
2021-04-13 03:06:04 +0500

    2. Анализ доступности сервера. [#482523] fix

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -51,8 +51,8 @@ public class Analizy {
         try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
             out.println("15:01:30;15:02:32");
             out.println("15:10:30;23:12:32");
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception el) {
+            el.printStackTrace();
         }
     }
 }
--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,9 +1,10 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,21 +13,35 @@ import static org.junit.Assert.*;
 
 public class AnalizyTest {
 
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
     @Test
-    public void whenNormal() {
-        new Analizy().unavailable("server.log", "target.txt");
-        List<String> rsl = new ArrayList<String>();
-        try (BufferedReader out = new BufferedReader(new FileReader("analyzed.txt"))) {
-            String curr = out.readLine();
-            while (curr != null) {
-                rsl.add(curr);
-                curr = out.readLine();
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void whenNormal() throws IOException {
+        File source = folder.newFile("source.log");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter writer = new PrintWriter(source)) {
+            writer.write(
+                    "200 10:56:01\n" +
+                    "200 10:57:01\n" +
+                    "400 10:58:01\n" +
+                    "200 10:59:01\n" +
+                    "500 11:01:02\n" +
+                    "200 11:02:02"
+            );
         }
-        List<String> exp = List.of("10:58:01;10:59:01", "11:01:02;11:02:02");
-        assertThat(rsl, is(exp));
+        new Analizy().unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(el -> {
+                String string = el + System.lineSeparator();
+                rsl.append(string);
+            });
+        }
+        String ex = "<10:58:01;10:59:01\n" +
+                "11:01:02;11:02:02\n" +
+                ">";
+        assertThat(rsl, is(ex));
     }
 
 }
