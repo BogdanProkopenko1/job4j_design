25f5b89669dfaccee17d5aa7f3ab7daf87adc19e
BogdanProkopenko1 <integer.args@gmail.com>
2021-04-28 21:47:39 +0500

    1. Бот [#482539]

---
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -0,0 +1,35 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoServer {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket server = new ServerSocket(9500)) {
+            while (!server.isClosed()) {
+                Socket socket = server.accept();
+                try (OutputStream out = socket.getOutputStream();
+                     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(socket.getInputStream()))) {
+                    String str;
+                    while (!(str = in.readLine()).isEmpty()) {
+                        System.out.println(str);
+                        if (str.startsWith("GET /?")) {
+                            String[] arguments = str.split("[= ]");
+                            if (arguments[2].equals("Exist")) {
+                                socket.close();
+                                server.close();
+                            } else if (arguments[2].equals("Hello")) {
+                                out.write("HTTP/1.1 200 HELLO\r\n".getBytes());
+                            } else {
+                                out.write("HTTP/1.1 200 WHAT\r\n".getBytes());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
