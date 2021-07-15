a80247acb54c8014d3496e88e4e264f7ec1e151d
BogdanProkopenko1 <integer.args@gmail.com>
2021-07-15 19:16:20 +0500

    0. Что такое база данных. [#6860 #66577] fix

---
+++ db/scripts/sql/gerAccounts.sql
@@ -0,0 +1,2 @@
+select * from accounts
+select a.mail почта, a.password пароль, a.acces разрешение from accounts a where id = 2
---
+++ db/scripts/sql/insertAccounts.sql
@@ -0,0 +1,2 @@
+insert into accounts(mail, password, acces) values('pvzarea@gmail.com', 145211, true) returning id
+insert into accounts(mail, password, acces) values('nbjeoijpvr@gmail.com', 161651, false)
