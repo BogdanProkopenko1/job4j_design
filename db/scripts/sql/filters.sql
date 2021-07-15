9b2c06622478b52fa905229c63fd7f4baf790961
BogdanProkopenko1 <integer.args@gmail.com>
2021-07-16 00:38:07 +0500

    Фильтры [#1732 #158840] fix

--- db/scripts/sql/filters.sql
+++ db/scripts/sql/filters.sql
@@ -1,22 +1,25 @@
-select * from product p join type t on t.name = 'СЫР' and p.type_id = t.id;
+select * from product p join type t on p.type_id = t.id where t.name = 'СЫР';
 
 select * from product p where p.name like '%мороженное%'
 
-select * from product p where extract(month from p.expired_date) = extract(month from current_date + interval '1 month') and extract(year from p.expired_date) = extract(year from current_date + interval '1 month');
+select
+	*
+from
+	product p
+where
+	date_part('month', expired_date) = date_part('month', now() + interval '1 month');
 
 select * from product where price = (select max(price) from product)
 
 select t.name as ТИП, count(*) as КОЛИЧЕСТВО from product p join type t on t.id = p.type_id group by t.name
 
-select * from product p join type t on (t.name = 'СЫР' or t.name = 'МОЛОКО') and p.type_id = t.id;
+select * from product p join type t on p.type_id = t.id where t.name = 'СЫР' or t.name = 'МОЛОКО';
 
 select
 	t.name as ТИП, count(*)
 from
 	product p
 join
-	type t on t.id = p.type_id group by t.name
-having
-	(select count(*) from product p join type t on t.id = p.type_id) < 10
+	type t on t.id = p.type_id group by t.name having COUNT(*) < 10
 
 select p.*, t.name as ТИП from product p join type t on p.type_id = t.id
