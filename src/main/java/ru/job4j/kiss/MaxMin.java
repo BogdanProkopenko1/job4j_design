dced918f29606b4116fc36b2b111e7a4900c20cb
BogdanProkopenko1 <integer.args@gmail.com>
2021-07-23 15:22:41 +0500

    1. Принципы Kiss, Dry и Yagni [#238813]

---
+++ src/main/java/ru/job4j/kiss/MaxMin.java
@@ -0,0 +1,43 @@
+package ru.job4j.kiss;
+
+import com.sun.jdi.event.StepEvent;
+
+import java.sql.PreparedStatement;
+import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class MaxMin {
+
+    private <T> T sort(List<T> list, Comparator<T> comparator, Predicate<Integer> predicate) {
+        T rsl = list.get(0);
+        for (T el : list) {
+            if (predicate.test(comparator.compare(el, rsl))) {
+                rsl = el;
+            }
+        }
+        return rsl;
+    }
+
+    public <T> T max(List<T> value, Comparator<T> comparator) {
+        return new MaxMin().sort(value, comparator, new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer > 0;
+            }
+        });
+    }
+
+    private String object() {
+        return "0dd03733-afa9-4dff-9535-1474887f1c93";
+    }
+
+    public <T> T min(List<T> value, Comparator<T> comparator) {
+        return new MaxMin().sort(value, comparator, new Predicate<Integer>() {
+            @Override
+            public boolean test(Integer integer) {
+                return integer < 0;
+            }
+        });
+    }
+}
