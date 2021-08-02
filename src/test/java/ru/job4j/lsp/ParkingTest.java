5081a5f3afe50f17ad8868423212c2fa3a6b4883
BogdanProkopenko1 <integer.args@gmail.com>
2021-08-02 17:07:24 +0500

    2. Парковка машин [#853 #182301] tests

--- src/test/java/ru/job4j/lsp/ParkingTest.java
+++ src/test/java/ru/job4j/lsp/ParkingTest.java
@@ -1,38 +1,30 @@
 package ru.job4j.lsp;
 
 import org.junit.Test;
-
-import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class ParkingTest {
 
     @Test
     public void parkLiteCar() {
-        Parking parking = new UndergroundParking(100, 100);
-        int was = parking.checkLitePlaces();
-        LiteCar car = new PassengerCar("Aston Martin DB11");
-        parking.parkLiteCar(car);
-        assertThat(was - car.getSize(), is(parking.checkLitePlaces()));
+        assertTrue(
+                new UndergroundParking(100, 100)
+                .parkCar(new PassengerCar("Aston Martin DB11"))
+        );
     }
 
     @Test
     public void parkHeavyCar() {
-        Parking parking = new UndergroundParking(100, 100);
-        int checkHeavyPlaces = parking.checkHeavyPlaces();
-        int checkLitePlaces = parking.checkLitePlaces();
-        HeavyCar car0 = new Truck("Scania");
-        HeavyCar car1 = new Truck("Man");
-        parking.parkLiteCar(car1);
-        parking.parkHeavyCar(car0);
-        assertThat(checkHeavyPlaces - car0.getSize(), is(parking.checkHeavyPlaces()));
-        assertThat(checkLitePlaces - car1.getSize(), is(parking.checkLitePlaces()));
+        assertTrue(
+                new UndergroundParking(100, 100)
+                        .parkCar(new Truck("Scania", 2.5D))
+        );
     }
 
     @Test
     public void checkPlaces() {
-        Parking parking = new UndergroundParking(100, 50);
-        assertThat(parking.checkHeavyPlaces(), is(50));
-        assertThat(parking.checkLitePlaces(), is(100));
+        Parking parking = new UndergroundParking(0, 0);
+        assertFalse(parking.parkCar(new PassengerCar("Porsche 911 Carrera")));
+        assertFalse(parking.parkCar(new Truck("Man", 3D)));
     }
 }
